84096c9e40b26e87d3768d1e0156dbc886d92250
Njoki Kamau <njokifaith61@gmail.com>
2020-06-16 16:48:25 +0300

    add index argument so each quote has its own functioning upvote and downvote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showTheme: boolean;
-    constructor(public id: number,public words: string,public theme: string, public submitter:string,public author:string, public publishDate: Date){
+    constructor(public id: number,public words: string,public theme: string, public submitter:string,public author:string, public publishDate: Date,public like: 0){
       this.showTheme=false;
     }
   }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1,"If at first you don't succeed, call it version 1.0", 'Perseverance', "Imani", 'Anonymous',new Date(2020,6,14)),
-    new Quote(2,"When life brings big winds of change that almost blow you over,close your eyes, hold on tight and believe", "Perseverance","Njoki",'Lisa Lieberman',new Date(2010,6,14)),
-    new Quote(3, "The elevator to success is out of order. You'll have to use the stairs one step at a time", "Perseverance","Kamau",'Joe Girald', new Date(2020,6,14)),
-    new Quote(4,"Learn from the mistakes of others. You can never live long enough to make them all yourself","Perseverance", "Faith",'Groucho Marx',new Date(2020,6,14)),
-    new Quote(5,"If at first you don't succeed, then skydiving definitely isn't for you","Perseverance","David",'Steven Wright',new Date(2020,6,14)),
-    new Quote(6,"Behind every good programmer is a frustrated programmer", 'Perseverance',"Mary",'Anonymous',new Date(2020,6,14)),
+    new Quote(1,"If at first you don't succeed, call it version 1.0", 'Perseverance', "Imani", 'Anonymous',new Date(2020,6,14),0),
+    new Quote(2,"When life brings big winds of change that almost blow you over,close your eyes, hold on tight and believe", "Perseverance","Njoki",'Lisa Lieberman',new Date(2010,6,14),0),
+    new Quote(3, "The elevator to success is out of order. You'll have to use the stairs one step at a time", "Perseverance","Kamau",'Joe Girald', new Date(2020,6,14),0),
+    new Quote(4,"Learn from the mistakes of others. You can never live long enough to make them all yourself","Perseverance", "Faith",'Groucho Marx',new Date(2020,6,14),0),
+    new Quote(5,"If at first you don't succeed, then skydiving definitely isn't for you","Perseverance","David",'Steven Wright',new Date(2020,6,14),0),
+    new Quote(6,"Behind every good programmer is a frustrated programmer", 'Perseverance',"Mary",'Anonymous',new Date(2020,6,14),0),
   ];
 
   toggleDetails(index){
@@ -38,22 +38,18 @@ export class QuoteComponent implements OnInit {
     this.quotes.push(quote)
   }
 
-  numberOfLikes: number =0;
-
-  likeButtonClick(){
-    this.numberOfLikes++;
-
-
-  }
-  dislikeButtonClick() {
-    this.numberOfLikes --;
-
-  }
+ 
   constructor() { }
  
   ngOnInit() {
   }
 
+  likeButtonClick(i: number) {
+   this.quotes[i].like += 1;
+ }
 
+ dislikeButtonClick(i: number) {
+  this.quotes[i].like -= 1;
+}
 }
  
