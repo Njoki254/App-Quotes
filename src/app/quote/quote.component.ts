29b7f4f804d79c8daea5fe68974b17c169bb9ac8
Njoki Kamau <njokifaith61@gmail.com>
2020-06-16 13:18:50 +0300

    make quote and submitter, and date display

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,21 +9,21 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1,"Imani","If at first you don't succeed, call it version 1.0", 'Anonymous', 'Perseverance',new Date(2020,6,14)),
-    new Quote(2,"Njoki","When life brings big winds of change that almost blow you over,close your eyes, hold on tight and believe", 'Lisa Lieberman', 'Perseverance',new Date(2010,6,14)),
-    new Quote(3,"Kamau", "The elevator to success is out of order. You'll have to use the stairs one step at a time", 'Joe Girald', 'Perseverance',new Date(2020,6,14)),
-    new Quote(4,"Faith","Learn from the mistakes of others. You can never live long enough to make them all yourself", 'Groucho Marx', 'Perseverance',new Date(2020,6,14)),
-    new Quote(5,"Maureen","If at first you don't succeed, then skydiving definitely isn't for you", 'Steven Wright', 'Perseverance',new Date(2020,6,14)),
-    new Quote(6,"Henry","Behind every good programmer is a frustrated programmer", 'Imani Kamau', 'Perseverance',new Date(2020,6,14)),
+    new Quote(1,"If at first you don't succeed, call it version 1.0", 'Perseverance', "Imani", 'Anonymous',new Date(2020,6,14)),
+    new Quote(2,"When life brings big winds of change that almost blow you over,close your eyes, hold on tight and believe", "Perseverance","Njoki",'Lisa Lieberman',new Date(2010,6,14)),
+    new Quote(3, "The elevator to success is out of order. You'll have to use the stairs one step at a time", "Perseverance","Kamau",'Joe Girald', new Date(2020,6,14)),
+    new Quote(4,"Learn from the mistakes of others. You can never live long enough to make them all yourself","Perseverance", "Faith",'Groucho Marx',new Date(2020,6,14)),
+    new Quote(5,"If at first you don't succeed, then skydiving definitely isn't for you","Perseverance","David",'Steven Wright',new Date(2020,6,14)),
+    new Quote(6,"Behind every good programmer is a frustrated programmer", 'Perseverance',"Mary",'Anonymous',new Date(2020,6,14)),
   ];
 
   toggleDetails(index){
-    this.quotes[index].showMore = !this.quotes[index].showMore;
+    this.quotes[index].showTheme = !this.quotes[index].showTheme;
   }
 
   deleteQuote(isUnwanted: any, index: number){
     if (isUnwanted) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].words}?`)
 
       if (toDelete){
         this.quotes.splice(index,1)
